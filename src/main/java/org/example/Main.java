91753fa40cbdbc78162166634d59783786325f6f
Kenny-R93 <krangelunc@gmail.com>
2023-06-02 20:41:00 -0400

    Dream as if you'll live forever, live as if you'll die today.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,64 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Creating an instance of UserService
+        UserService userService = new UserService();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Creating a new User
+        User newUser = new User("JohnDoe", "password", "johndoe@example.com");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Registering the user
+        boolean registrationSuccessful = userService.registerUser(newUser);
+        if (registrationSuccessful) {
+            System.out.println("User registration successful!");
+        } else {
+            System.out.println("User registration failed. Username might already be taken.");
+        }
+
+        // Logging in the user
+        User loggedInUser = userService.loginUser("JohnDoe", "password");
+        if (loggedInUser != null) {
+            System.out.println("User login successful!");
+        } else {
+            System.out.println("User login failed. Username and password do not match.");
+        }
+
+        // Creating an instance of BookService
+        BookService bookService = new BookService();
+
+        // Adding some books to the "database"
+        bookService.addBook(new Book("1984", "George Orwell", "Dystopian", 9.99));
+        bookService.addBook(new Book("To Kill a Mockingbird", "Harper Lee", "Fiction", 14.99));
+
+        // Searching for a book
+        List<Book> books = bookService.searchBook("1984");
+        if (!books.isEmpty()) {
+            System.out.println("Books found:");
+            for (Book book : books) {
+                System.out.println(book.getTitle() + " by " + book.getAuthor());
+            }
+        } else {
+            System.out.println("No books found with the given keyword.");
+        }
+
+        // Purchasing a book
+        Book bookToPurchase = books.get(0);
+        boolean purchaseSuccessful = bookService.purchaseBook(loggedInUser, bookToPurchase);
+        if (purchaseSuccessful) {
+            System.out.println("Book purchase successful!");
+        } else {
+            System.out.println("Book purchase failed. The book might not be available.");
+        }
+
+        // Adding a book review
+        boolean reviewAdded = bookService.addBookReview(loggedInUser, bookToPurchase, "Amazing book!");
+        if (reviewAdded) {
+            System.out.println("Review added successfully!");
+        } else {
+            System.out.println("Failed to add review. The user might not have purchased this book.");
         }
     }
 }
